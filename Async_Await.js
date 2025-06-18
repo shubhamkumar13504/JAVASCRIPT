fa025bcff10dad63e487072ff2a86a959321054e
SHUBHAM KUMAR <shubhamkumar13504@gmail.com>
2025-06-18 17:09:36 +0530

    UPDATED

--- Async_Await.js
+++ Async_Await.js
@@ -1,3 +1,26 @@
-async function Newfunc(params) {
+async function Newfunc() {
+    let p=new Promise((resolve,reject)=>{
+    setTimeout(() => {
+        // reject('reject')
+        resolve('54362')
+    }, 3000);
     
+})
+let p2=new Promise((resolve,reject)=>{
+    setTimeout(() => {
+        // reject('reject')
+        resolve('984567')
+    }, 5000);})
+    console.log("data fetching...")
+    let a =await p
+    console.log("data fetched 'p'...")
+    console.log("data fetching p2...")
+    let b =await p2
+    console.log("data fetched p2...")
+    return [a,b]
 }
+let a= Newfunc()
+a.then((v)=>{
+    console.log(v);
+    
+})
